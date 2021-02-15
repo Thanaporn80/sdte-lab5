a05a454bd0f558a0fc98d2097b6ad58cf6ab1236
Thanaporn80 <61070080@kmitl.ac.th>
2021-02-15 18:32:50 +0700

    Lab5 final

--- src/server.js
+++ src/server.js
@@ -1,8 +1,8 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 
-app.get('/student_id', (req, res) => {
-    res.send('61070080')
-})
+app.get("/student_id", (req, res) => {
+  res.send("61070080");
+});
 
-module.exports = app
+module.exports = app;
